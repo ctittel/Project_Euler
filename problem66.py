17bb8a385330bac2f29b01d59e917262933fbb9b
Christoph <christoph.tittel@gmail.com>
2020-03-14 18:16:01 +0100

    finished problem 66 with fractals

--- problem66.py
+++ problem66.py
@@ -1,71 +1,84 @@
 import math
-import time
-from sortedcontainers import SortedSet
 
-# squares = set([x**2 for x in range(1,1000000)])
+def num_plus_frac(a,frac):
+    """ Input: a + (b/c)
+        Output: (num, denum) """
+    return (a*frac[1] + frac[0], frac[1])
 
-def is_square_old(x):
-    # print(x)
-    return ((int(math.sqrt(x))**2) == x)
+def num_div_frac(a,frac):
+    """ a / (b/c) """
+    return (frac[1] * a, frac[0])
 
-def is_square(n):
-    return round(n ** 0.5) ** 2 == n
+def calc_next(x, prev_pair):
+    # print('calc_next INPUT ', prev_pair)
+    b_prev = prev_pair[1]
+    c_prev = prev_pair[2]
 
-def is_square_babylon(num):
-  x = num // 2
-  seen = set([x])
-  while x * x != num:
-    x = (x + (num // x)) // 2
-    if x in seen: return False
-    seen.add(x)
-  return True
+    if not b_prev:
+        return prev_pair
 
-def get_min_x(squares, D):
-    for y_squared in squares:
-        x_squared = y_squared*D + 1
-        if is_square(x_squared):
-            return float(x_squared)
-    return False
+    b = int((x - (c_prev**2))/b_prev)
 
-def function_test(fun, input):
-    start = time.time()
-    for i in input:
-        a = fun(i)
-    print("Function ", fun, " took ", time.time() -start, " seconds to process ", len(input), " items")
+    c = math.floor(math.sqrt(x))
+    a = 0
 
-# function_test(is_square, range(1000000,10000000))
-# function_test(is_square2, range(1000000,10000000))
-# exit()
+    while b:
+        s = c_prev + c
+        # print("s = ",s, " b = ", b)
+        if ((s//b) == (s/b)):
+            a = s//b
+            break
+        c -= 1
 
-best_D = None
+    assert a >= 0
+    assert b >= 0
+    assert c >= 0
+    # print('calc_next OUTPUT ', (a, b, c))
+    return (a, b, c)
+
+def calc_solution(D):
+    """ x^2 - D * y^2 = 1 - return (x,y) that are integers """
+    a0 = math.floor(math.sqrt(D))
+
+    # skip rational ones
+    if a0**2 == D:
+        return None
+
+    pairs = [calc_next(D, (a0, 1, a0))]
+    num = 1
+    denum = pairs[-1][0]
+
+    x = (num+a0*denum)
+    y = denum
+
+    while x**2 - D * y**2 != 1:
+        pairs.append(calc_next(D, pairs[-1]))
+        next_a = pairs[-1][0]
+
+        num, denum = num_div_frac(1, num_plus_frac(next_a, (num, denum)))
+
+        x = (num+a0*denum)
+        y = denum
+    return x,y
+
+
+def is_square(x):
+    return int(math.sqrt(x))**2 == x
+
+
 best_x = 0
+best_D = None
 
-y_lower = 2
-y_upper = 1000
+for D in range(2,1001):
+    print("Calculating D = ", D)
+    if is_square(D):
+        continue
 
-candidates = [D for D in range(3,1001)]
-while len(candidates) > 1:
-    print("num candidates: ", len(candidates), " y_lower = ", y_lower, " y_upper = ", y_upper)
-    new_candidates = []
-    for D in candidates:
-        if is_square(D):
-            continue
-        found = False
-        for y in range(y_lower, y_upper):
-            y_squared = y**2
-            x_squared = D*y_squared + 1
-            if is_square(x_squared):
-                print("D = ", D, " x square = ", x_squared)
-                found = True
-                if best_x < x_squared:
-                    best_x = x_squared
-                    best_D = D
-                break
-        if not found:
-            new_candidates.append(D)
-    y_lower = y_upper
-    y_upper *= 10
-    candidates = new_candidates
+    x, y = calc_solution(D)
 
-print(candidates)
-print(best_D)
+    print("D = ", D, "   x = ", x)
+    if x > best_x:
+        best_x = x
+        best_D = D
+
+print("best_x = ", best_x, " for best_D = ", best_D)

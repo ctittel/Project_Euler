90090fd224d6fcca07246fe275a0009c5e69aac1
Christoph <christoph.tittel@gmail.com>
2020-03-14 17:25:59 +0100

    problem 66 - bruteforce

--- problem66.py
+++ problem66.py
@@ -1,11 +1,15 @@
 import math
 import time
+from sortedcontainers import SortedSet
 
 # squares = set([x**2 for x in range(1,1000000)])
 
 def is_square_old(x):
     # print(x)
     return ((int(math.sqrt(x))**2) == x)
+
+def is_square(n):
+    return round(n ** 0.5) ** 2 == n
 
 def is_square_babylon(num):
   x = num // 2
@@ -15,12 +19,6 @@ def is_square_babylon(num):
     if x in seen: return False
     seen.add(x)
   return True
-  
-all_squares = set([i**2 for i in range(2, 10000000)])
-
-def is_square(x):
-    global all_squares
-    return x in all_squares
 
 def get_min_x(squares, D):
     for y_squared in squares:
@@ -29,39 +27,45 @@ def get_min_x(squares, D):
             return float(x_squared)
     return False
 
+def function_test(fun, input):
+    start = time.time()
+    for i in input:
+        a = fun(i)
+    print("Function ", fun, " took ", time.time() -start, " seconds to process ", len(input), " items")
 
-# for D in range(3, 1001):
-#     # skip if D is square
-#     if is_square(D):
-#         continue
-
-#     print(D, get_min_x(D))
-#     # print(get_min_x(D))
+# function_test(is_square, range(1000000,10000000))
+# function_test(is_square2, range(1000000,10000000))
+# exit()
 
 best_D = None
 best_x = 0
 
-candidates = [D for D in range(3,1001)]
-squares_min = 1
-squares_max = 10
-while len(candidates) > 1:
-    print("Starting new loop with squares min = ", squares_min, " squares max = ", squares_max, " num candidates = ", len(candidates))
-    new_candidates = []
-    squares = [x**2 for x in range(squares_min,squares_max)]
-    print("... calculated squares list")
-    for D in candidates:
-        x = get_min_x(squares, D)
-        if not x:
-            new_candidates.append(D)
-        elif x > best_x:
-            best_x = x
-            best_D = D
-            print("..... new highest x = ", best_x, " for D = ", best_D)
-    candidates = new_candidates
-    squares_min *= 10
-    squares_max *= 10
-    # print("suqares min = ", squares_min, " squares max = ", squares_max, " len candidates = ", len(candidates))
-    print("... loop done")
+y_lower = 2
+y_upper = 1000
 
-print("highest x = ", best_x, " for D = ", best_D)
-print("Candidates = ", candidates)
+candidates = [D for D in range(3,1001)]
+while len(candidates) > 1:
+    print("num candidates: ", len(candidates), " y_lower = ", y_lower, " y_upper = ", y_upper)
+    new_candidates = []
+    for D in candidates:
+        if is_square(D):
+            continue
+        found = False
+        for y in range(y_lower, y_upper):
+            y_squared = y**2
+            x_squared = D*y_squared + 1
+            if is_square(x_squared):
+                print("D = ", D, " x square = ", x_squared)
+                found = True
+                if best_x < x_squared:
+                    best_x = x_squared
+                    best_D = D
+                break
+        if not found:
+            new_candidates.append(D)
+    y_lower = y_upper
+    y_upper *= 10
+    candidates = new_candidates
+
+print(candidates)
+print(best_D)
